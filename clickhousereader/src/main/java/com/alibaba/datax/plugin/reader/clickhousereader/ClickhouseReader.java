914532fe4470c874f0425a7d9ff92dc7c0c2d80e
donghao <donghao@mobvista.com>
2024-01-05 16:39:29 +0800

    保持本地变更

--- clickhousereader/src/main/java/com/alibaba/datax/plugin/reader/clickhousereader/ClickhouseReader.java
+++ clickhousereader/src/main/java/com/alibaba/datax/plugin/reader/clickhousereader/ClickhouseReader.java
@@ -2,74 +2,93 @@ package com.alibaba.datax.plugin.reader.clickhousereader;
 
 import java.util.List;
 
+import com.alibaba.datax.common.exception.DataXException;
+import com.alibaba.datax.common.plugin.RecordSender;
+import com.alibaba.datax.plugin.rdbms.reader.CommonRdbmsReader;
+import com.alibaba.datax.plugin.rdbms.reader.Constant;
+import com.alibaba.datax.plugin.rdbms.util.DBUtilErrorCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.alibaba.datax.common.plugin.RecordSender;
 import com.alibaba.datax.common.spi.Reader;
 import com.alibaba.datax.common.util.Configuration;
-import com.alibaba.datax.plugin.rdbms.reader.CommonRdbmsReader;
 import com.alibaba.datax.plugin.rdbms.util.DataBaseType;
 
-public class ClickhouseReader extends Reader {
+/**
+ * @author : donghao
+ * @version : 1.0
+ * @className : ClickHouseReader
+ * @date : 2022-07-20 16:14
+ */
+public class ClickHouseReader extends Reader {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ClickHouseReader.class);
     private static final DataBaseType DATABASE_TYPE = DataBaseType.ClickHouse;
-    private static final Logger LOG = LoggerFactory.getLogger(ClickhouseReader.class);
 
     public static class Job extends Reader.Job {
-        private Configuration jobConfig = null;
+        private Configuration originalConfig;
         private CommonRdbmsReader.Job commonRdbmsReaderMaster;
 
         @Override
         public void init() {
-            this.jobConfig = super.getPluginJobConf();
+            this.originalConfig = super.getPluginJobConf();
+            int fetchSize = this.originalConfig.getInt(Constant.FETCH_SIZE, Constant.DEFAULT_FETCH_SIZE);
+            if (fetchSize < 1) {
+                throw DataXException.asDataXException(DBUtilErrorCode.REQUIRED_VALUE,
+                        String.format("您配置的fetchSize有误，根据DataX的设计，fetchSize : [%d] 设置值不能小于 1.", fetchSize));
+            }
+            this.originalConfig.set(Constant.FETCH_SIZE, fetchSize);
             this.commonRdbmsReaderMaster = new CommonRdbmsReader.Job(DATABASE_TYPE);
-            this.commonRdbmsReaderMaster.init(this.jobConfig);
+            this.commonRdbmsReaderMaster.init(this.originalConfig);
+            LOG.info("ClickHouseReader Job初始化成功");
         }
 
         @Override
         public List<Configuration> split(int mandatoryNumber) {
-            return this.commonRdbmsReaderMaster.split(this.jobConfig, mandatoryNumber);
+            return this.commonRdbmsReaderMaster.split(this.originalConfig, mandatoryNumber);
         }
 
         @Override
         public void post() {
-            this.commonRdbmsReaderMaster.post(this.jobConfig);
+            this.commonRdbmsReaderMaster.post(this.originalConfig);
         }
 
         @Override
         public void destroy() {
-            this.commonRdbmsReaderMaster.destroy(this.jobConfig);
+            this.commonRdbmsReaderMaster.destroy(this.originalConfig);
         }
     }
 
     public static class Task extends Reader.Task {
 
-        private Configuration jobConfig;
-        private CommonRdbmsReader.Task commonRdbmsReaderSlave;
+        private Configuration taskConfig;
+
+        private ClickHouseReaderTask clickHouseReaderTask;
+
 
         @Override
         public void init() {
-            this.jobConfig = super.getPluginJobConf();
-            this.commonRdbmsReaderSlave = new CommonRdbmsReader.Task(DATABASE_TYPE, super.getTaskGroupId(), super.getTaskId());
-            this.commonRdbmsReaderSlave.init(this.jobConfig);
-        }
-
-        @Override
-        public void startRead(RecordSender recordSender) {
-            int fetchSize = this.jobConfig.getInt(com.alibaba.datax.plugin.rdbms.reader.Constant.FETCH_SIZE, 1000);
-
-            this.commonRdbmsReaderSlave.startRead(this.jobConfig, recordSender, super.getTaskPluginCollector(), fetchSize);
-        }
-
-        @Override
-        public void post() {
-            this.commonRdbmsReaderSlave.post(this.jobConfig);
+            this.taskConfig = super.getPluginJobConf();
+            clickHouseReaderTask = new ClickHouseReaderTask(DATABASE_TYPE, super.getTaskGroupId(), super.getTaskId());
+            clickHouseReaderTask.init(taskConfig);
+            LOG.info("ClickHouseReader Task初始化成功");
         }
 
         @Override
         public void destroy() {
-            this.commonRdbmsReaderSlave.destroy(this.jobConfig);
+            clickHouseReaderTask.destroy(taskConfig);
+        }
+
+        @Override
+        public void startRead(RecordSender recordSender) {
+            int fetchSize = this.taskConfig.getInt(Constant.FETCH_SIZE);
+            clickHouseReaderTask.startRead(taskConfig, recordSender, super.getTaskPluginCollector(), fetchSize);
+        }
+
+        @Override
+        public void post() {
+            clickHouseReaderTask.post(taskConfig);
         }
     }
+
 }
